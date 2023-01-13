1e0b36e2d7ba9776341116f3c541c1ea7b4bb7a7
santosh patel <109598196+santoshpatel4157@users.noreply.github.com>
2023-01-13 21:10:04 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,20 @@
- import React from 'react';
+import React from 'react';
 
- export const Todo  = (props) =>{
-    return(
-        <>
-            <div className="todo" id="todo-${id}">
-                {props.id}
-                <div className='todo-title'>
-                    {props.title}
-                    <div className='todo-status'>
-                    {props.completed}
+export const Todo = (e) => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <div className='todo' id={`todo-${e.id}`}>
+                        <div className='todo-title'>
+                            {e.title}
+                        </div>
+                        <div className='todo-status'>
+                            {e.completed ? "Completed" : "InCompleted"}
+                        </div>
                     </div>
-                </div>
-            </div>
-            
-        </>
-    )      
- }
+                </li>
+            </ul>
+        </div>
+    )
+}
